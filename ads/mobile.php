f0acccf48e94ca438fec1e225fadba4ee35ebfc2
Alex Montaña <alex@worldx3.com>
2018-12-22 08:44:33 +0100

    Edited tagmanager.

--- ads/mobile.php
+++
@@ -1,6 +0,0 @@
-<div class="ads" style="width:320px;height:100px;">
-  <ins class="adsbygoogle"
-     style="display:inline-block;width:320px;height:100px"
-     data-ad-client="ca-pub-1300504758348232"
-     data-ad-slot="3980145601"></ins>
-</div>
